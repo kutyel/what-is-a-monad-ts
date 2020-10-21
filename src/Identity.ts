58970511eeac79fc48be6f850387e813415daea7
Flavio Corpa <flavio.corpa@47deg.com>
2020-10-21 13:33:57 +0200

    Simplify Identity implementation https://github.com/gcanti/fp-ts/blob/74093f7111196e790f3018c03a03e7aaba239cd9/docs/guides/HKT.md

--- src/Identity.ts
+++ src/Identity.ts
@@ -1,43 +1,34 @@
 import { Eq } from 'fp-ts/lib/Eq'
+import { Functor1 } from 'fp-ts/lib/Functor'
 import { Applicative1 } from 'fp-ts/lib/Applicative'
 import { Monad1 } from 'fp-ts/lib/Monad'
 
 import { Arbitrary } from 'fast-check'
 
 // type encoding
+type Identity<A> = A
 
 const URI = 'Identity'
 type URI = typeof URI
 
-interface Identity<A> {
-  readonly _tag: 'Identity'
-  readonly value: A
-}
-
 declare module 'fp-ts/lib/HKT' {
   interface URItoKind<A> {
-    readonly [URI]: Identity<A>
+    readonly Identity: Identity<A>
   }
 }
 
 // constructor
-
-const iden = <A>(a: A): Identity<A> => ({ _tag: 'Identity', value: a })
+const id = <A>(x: A): A => x
 
 // operations
-
-const of: Applicative1<URI>['of'] = iden
-const map: Monad1<URI>['map'] = (fa, f) => iden(f(fa.value))
-const ap: Monad1<URI>['ap'] = (fab, fa) => iden(fab.value(fa.value))
-const chain: Monad1<URI>['chain'] = (ma, f) => f(ma.value)
+const map: Functor1<URI>['map'] = (fa, f) => f(fa)
+const of: Applicative1<URI>['of'] = id
+const ap: Applicative1<URI>['ap'] = (fab, fa) => fab(fa)
+const chain: Monad1<URI>['chain'] = (ma, f) => f(ma)
 
 // needed for tests
-
-export const getEq = <A>(E: Eq<A>): Eq<Identity<A>> => ({
-  equals: (x, y) => x === y || E.equals(x.value, y.value)
-})
-
-export const getIdentity = <A>(arb: Arbitrary<A>): Arbitrary<Identity<A>> => arb.map(iden)
+export const getEq = <A>(): Eq<Identity<A>> => ({ equals: (x, y) => x === y })
+export const getIdentity = <A>(arb: Arbitrary<A>): Arbitrary<Identity<A>> => arb.map(id)
 
 // main export
 export const identity: Monad1<URI> = { URI, map, of, ap, chain }
